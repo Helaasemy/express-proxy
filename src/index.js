9c04a92296ad3babafa3a3f79cb138f2e87f6d00
Hamza Elaasemy <hamzaelaasemy@gmail.com>
2021-01-21 19:38:48 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT||8080;
+
+const fetchJSON = (url) => fetch(url).then((res) => res.json());
+const baseUrl = "https://xkcd.com";
+
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  const url = `${baseUrl}/info.0.json`;
+  const data = await fetchJSON(url);
+  return res.json(data);
+});
+
+app.get("/:comicId", async (req, res) => {
+  const url = `${baseUrl}/${req.params.comicId}/info.0.json`;
+  const data = await fetchJSON(url);
+  return res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
